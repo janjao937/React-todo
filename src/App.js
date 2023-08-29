bca0ad34cd5cbd195bafd43e6eb5d511ed4105e5
janjao937 <janjao937@gmail.com>
2023-08-29 14:04:24 +0700

    cleanup Templete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import logo from "BanaKim.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          อยากเรียน <code>React</code> ไหม
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a className="App-link"></a>
+      
     </div>
   );
 }
