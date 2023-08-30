b42287354447e25ff1f7d19d6b7da5a22bd0cbbe
janjao937 <janjao937@gmail.com>
2023-08-30 19:34:29 +0700

    update todo component

--- src/Component/TodoComponent/TodoCreate.jsx
+++ src/Component/TodoComponent/TodoCreate.jsx
@@ -1,8 +1,11 @@
-import style from "../TodoComponent/TodoCreate.scss";
+import style from "../TodoComponent/TodoCreate.module.scss";
 
 const TodoCreate =()=>{
     return(
-        <div>Todo Create</div>
+        <div className={style.todo__create}>
+            <div className={style.todo__create__btn}>+</div>
+            <h3 className={style.todo__create__text}>Todo</h3>
+        </div>
     );
 }
 export default TodoCreate;  
--- src/Component/TodoComponent/TodoList.jsx
+++ src/Component/TodoComponent/TodoList.jsx
@@ -1,8 +1,18 @@
-import style from "./TodoList.scss";
+import style from "./TodoList.module.scss";
 
 const TodoList = ()=>{
     return(
-        <div>TodoList</div>
+        <ul className={style.todo__lists}>
+        <li className={style.todo}>
+          <span className={style.todo__checkbox}></span>
+          <p className={style.todo__task}>TodoItem 1 </p>
+          <span classNam e={style.todo__date}>30 AUG</span>
+          <div className={style.todo__action}>
+            <span className={style.todo__edit}></span>
+            <span className={style.todo__delete}></span>
+          </div>
+        </li>
+      </ul>
     );
 }
 
