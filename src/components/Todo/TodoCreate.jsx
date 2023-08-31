8a80192967938cb78a51308719676bb0abec8af7
janjao937 <janjao937@gmail.com>
2023-08-31 11:03:43 +0700

    add codition Render on TodoCreate

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,47 @@
 import styles from './TodoCreate.module.scss';
+import TodoForm from "./TodoForm"
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
+import { useState } from 'react';
+
+
+/*
+#codition rendering
+-default :show button & text
+-active : show TodoForm
+#event handilng 
+-เอาฟังค์ชั่นไปผูกติดกับ UI เพื่อให้ user เป็นคนเรียกเอง
+#Js value ไม่สามารถทำให้ React re-render ได้
+# useState (hook FN)
+-[state,setState]=useState(initialValue)
+-state = state
+-setState = FN ในการ change stateValue
+*/
 
 function TodoCreate() {
+  //Hook
+  const [isOpenForm,setIsOpenForm] = useState(false);
+  
+  const handleClick =()=>{
+    setIsOpenForm(!isOpenForm);
+    console.log("Banana"+isOpenForm);
+  }
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
+ 
+    <>  
+    {
+      isOpenForm?<TodoForm/>
+      :(
+        <div onClick={handleClick} className={styles.todo__create}>
+        <div className={styles.todo__create__button}>
+          <HiPlus />
+        </div>
+        <h3 className={styles.todo__create__text}>Add Task</h3>
       </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+      )
+    }
+    
+    </>
   );
 }
 
